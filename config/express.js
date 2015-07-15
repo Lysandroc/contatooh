00443778e340f48899977d10920401187b4e6567
Lysandro Carioca Braga <lysandroc@gmail.com>
2015-07-14 22:58:09 -0300

    Criacao de servicos em restful, criacao das rotas e controllers e views(utilizando o ejs), além disto foi adicionado e configurado alguns middlewares.

---
+++ config/express.js
@@ -0,0 +1,23 @@
+var express = require('express')
+  , load = require('express-load') 	
+  , methodOverride = require('method-override')()
+  , bodyParser = require('body-parser');
+
+module.exports = function() {
+	
+	var app = express();
+	app.set('port',3000);
+	app.use(express.static('./public'));
+	app.set('view engine','ejs');
+	app.set('views','./app/views');
+	app.use(bodyParser.urlencoded({extended: true}));
+	app.use(bodyParser.json()); 
+	app.use(methodOverride);
+	
+	load('models', {cwd: 'app'})
+		.then('controllers')
+		.then('routes')
+		.into(app);
+		
+	return app;	
+};
