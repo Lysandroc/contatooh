eb275ea7200ab21f636d14f96f39fddecf180775
Lysandro Carioca Braga <lysandroc@gmail.com>
2015-07-17 15:11:47 -0300

    consumindo um servico do expressjs atraves da injecao de dependencia do $http do angularjs

--- public/js/controllers/ContatosController.js
+++ public/js/controllers/ContatosController.js
@@ -1,28 +1,19 @@
 angular.module('contatooh').controller('ContatosController', 
-	function($scope) {
-		$scope.total =0;
-		$scope.incrementa = function() {
-			$scope.total++;	
-		};
+	function($scope, $http) {
 		
 		$scope.filtro='';
-		$scope.contatos = [
-			{ 	"_id": 1,
-				"nome" : "Contato angular 1",
-				"email": "cont1@empresa.com.br"
-			},
-			{ 	"_id": 2,
-				"nome" : "Contato angular 2",
-				"email": "cont2@empresa.com.br"
-			},
-			{ 	"_id": 3,
-				"nome" : "Contato angular 3",
-				"email": "cont3@empresa.com.br"
-			},
-			{ 	"_id": 4,
-				"nome" : "Lysandro Carioca",
-				"email": "lysandroc@"
-			}
-		];
+		$scope.contatos = [];
+		
+		$http.get('/contatos')
+		.success(function(data) {
+			$scope.contatos = data;
+		}).error(function(statusText) {
+			console.log('Nao foi possivel ler a lista dos contatos.');
+			console.log(statusText);
+		});
 	}
 );	
+
+
+
+
--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,16 @@
-angular.module('contatooh', ['ngRoute'])
+angular
+.module('contatooh', ['ngRoute'])
 	.config(function($routeProvider){
+		
 		$routeProvider.when('/contatos', {
 			templateUrl: 'Partials/contatos.html',
 			controller: 'ContatosController'
 		});
+		
 		$routeProvider.when('/contato/:contatoId', {
 			templateUrl: 'Partials/contato.html',
 			controller: 'ContatoController'
 		});
+		
 		$routeProvider.otherwise({redirectTo: '/contatos'});
 	});
