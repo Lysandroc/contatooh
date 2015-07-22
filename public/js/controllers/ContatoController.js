9967ffd5266271155ae660366718ec86f8a0b6bc
Lysandro Carioca Braga <lysandroc@gmail.com>
2015-07-21 23:45:41 -0300

    criacao da tela dos cadastros de contato.

--- public/js/controllers/ContatoController.js
+++ public/js/controllers/ContatoController.js
@@ -1,4 +1,50 @@
 angular.module('contatooh').controller('ContatoController',  
-	function($scope, $routeParams) {
-		console.log($routeParams.contatoId);	
+	function($scope, $routeParams, $resource) {
+			
+		var Contato = $resource('/contatos/:id');
+		
+		if ($routeParams.contatoId) {
+			var promise = Contato.get({id: $routeParams.contatoId}).$promise;
+			promise.then(function(contato) {
+				$scope.contato = contato;
+			}, function(erro){
+				$scope.mensagem = {
+					texto: 'Nao foi possivel obter o contato do cliente.'	
+				};
+				console.log(erro);
+			});	
+		} else {
+			$scope.contato = new Contato();
+		}
+		
+		$scope.salva = function() {
+			
+			// var promise = Contato.save($scope.contato).$promise;
+			// promise.then(function(){
+			// 	$scope.mensagem = {
+			// 		texto: 'Salvo com sucesso.'
+			// 	};
+			// 	$scope.contato = new Contato();
+			// 	console.log('salvo');
+			// }, function(erro){
+			// 	$scope.mensagem = {
+			// 		texto: 'Nao foi possivel salvar.'
+			// 	};
+			// 	console.log(erro);
+			// });
+			
+			var promise = $scope.contato.$save();
+			promise.then(function(){
+				$scope.mensagem = {
+					texto: 'Salvo com sucesso.'
+				};
+				$scope.contato = new Contato();
+				console.log('salvo');
+			}, function(erro){
+				$scope.mensagem = {
+					texto: 'Nao foi possivel salvar.'
+				};
+				console.log(erro);
+			});
+		};
 });
--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,11 @@
 angular.module('contatooh', ['ngRoute', 'ngResource'])
 	.config(function($routeProvider) {
 		
+		$routeProvider.when('/contato', {
+			templateUrl: 'Partials/contato.html',
+			controller: 'ContatoController'
+		});
+		
 		$routeProvider.when('/contatos', {
 			templateUrl: 'Partials/contatos.html',
 			controller: 'ContatosController'
