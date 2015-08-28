f2b57e4eac0474dcce654381d78fad8e937b087f
Lysandro Carioca Braga <lysandroc@gmail.com>
2015-08-27 22:14:27 -0300

    refatorando outra spec da lista de contatos utilizando o padrao PageObject

--- test/e2e/contatosPageSpec.js
+++ test/e2e/contatosPageSpec.js
@@ -1,24 +1,27 @@
+var ContatosPage = new require('./pages/contatosPage');
+
 describe('Pagina principal', function() {
 
+	var pagina = new ContatosPage();
+
 	beforeEach(function() {
-		browser.get('http://localhost:3000/#/contatos');
+			pagina.visitar();
 	});
 
 	it('Deve estar logado!', function() {
-		element(by.id('usuario-logado')).getText()
-		.then(function(texto) {
-			expect(texto.trim().length).toBeGreaterThan(0);
-		});
+			pagina.obterUsuarioLogado().then(function(texto) {
+					expect(texto.trim().length).toBeGreaterThan(0);
+			});
 	});
 
 	it('Deve remover um contato da lista', function() {
 
-		var totalAntes = element.all(by.repeater("contato in contatos")).count();
+		var totalAntes = pagina.obterTotalDeItensDaLista();
 
 		element(by.repeater("contato in contatos").row(0)
 		).element(by.css(".btn")).click();
 
-		var totalDepois = element.all(by.repeater('contato in contatos')).count();
+		var totalDepois = pagina.obterTotalDeItensDaLista();
 
 		expect(totalDepois).toBeLessThan(totalAntes);
 	});
