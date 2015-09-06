1dbf405de5fe43696e8ca801a1a682f67e40216c
Lysandro Carioca Braga <lysandroc@gmail.com>
2015-09-06 13:41:46 -0300

    correcao da rota

--- config/protractor.js
+++ config/protractor.js
@@ -12,13 +12,13 @@ exports.config = {
 	specs: ['../test/e2e/**/*.js'],
 
 	onPrepare: function() {
-		browser.driver.get('http://localhost:3000')
-		.then(function() {
-			//element(by.id('entrar')).click();
-			browser.driver.findElement(by.id('entrar')).click();
-			browser.driver.findElement(by.id('login_field')).sendKeys(config.seleniumUser);
-			browser.driver.findElement(by.id('password')).sendKeys(config.seleniumUserPassword);
-			browser.driver.findElement(by.name('commit')).click();
-		});
+    browser.driver.get('http://localhost:3000/#/auth').then(function() {
+      browser.driver.findElement(by.id('entrar')).click();
+      browser.driver.findElement(by.id('login_field'))
+      	.sendKeys(config.seleniumUser);
+      browser.driver.findElement(by.id('password'))
+      	.sendKeys(config.seleniumUserPassword);
+      browser.driver.findElement(by.name('commit')).click();
+    });
 	}
 };
