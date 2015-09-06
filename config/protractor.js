b5ccd8e6601b4450d849ed50aa8f1c62b128d5c1
Lysandro Carioca Braga <lysandroc@gmail.com>
2015-09-05 21:32:28 -0300

    teste

--- config/protractor.js
+++ config/protractor.js
@@ -1,14 +1,14 @@
 var config = require('./config')();
 
 exports.config = {
-	sauceUser: config.sauceUser,
-	sauceKey: config.sauceKey,
-	capabilities: {
-		'name': config.sauceTestName,
+	sauceUser : config.sauceUser,
+  sauceKey : config.sauceKey,
+  capabilities : {
+    'name': config.sauceTestName,
 		'browserName': 'chrome',
 		'tunnel-identifier': config.travisJobNumber,
 		'build': config.travisBuild
-	},
+   },
 	specs: ['../test/e2e/**/*.js'],
 
 	onPrepare: function() {
