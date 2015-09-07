a56fc04069b0fec56584323628f704a37fab9038
Lysandro Carioca Braga <lysandroc@gmail.com>
2015-09-07 10:03:26 -0300

    teste
    verificando o problema ao fazer os testes e2e

--- config/protractor.js
+++ config/protractor.js
@@ -1,4 +1,5 @@
 var config = require('./config')();
+
 exports.config = {
   sauceUser : config.sauceUser,
   sauceKey : config.sauceKey,
@@ -8,13 +9,16 @@ exports.config = {
     'tunnel-identifier': config.travisJobNumber,
     'build': config.travisBuild
   },
-
-  specs: ['../test/e2e/**/*Spec.js'],
+  specs: ['../test/e2e/**/*.js'],
   onPrepare: function() {
-      browser.driver.get('http://localhost:3000/#/auth');
+    browser.driver.get('http://localhost:3000/#/auth').then(function() {
       browser.driver.findElement(by.id('entrar')).click();
-      browser.driver.findElement(by.id('login_field')).sendKeys('lysandroc');
-      browser.driver.findElement(by.id('password')).sendKeys('mean.2016');
+      browser.driver.findElement(by.id('login_field'))
+      	.sendKeys(config.seleniumUser);
+      browser.driver.findElement(by.id('password'))
+      	.sendKeys(config.seleniumUserPassword);
       browser.driver.findElement(by.name('commit')).click();
+    });
+
   }
 };
