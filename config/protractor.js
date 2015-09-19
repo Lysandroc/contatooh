b0e65113da242cdd6ca9ab00611d8daf832edb4c
Lysandro Carioca Braga <lysandroc@gmail.com>
2015-09-18 21:52:56 -0300

    teste 01
    teste 01

--- config/protractor.js
+++ config/protractor.js
@@ -13,7 +13,7 @@ exports.config = {
   specs: ['../test/e2e/**/*Spec.js'],
 
   onPrepare: function() {
-    browser.driver.get('http://localhost:3000');
+    browser.driver.get('http://localhost:3000/#/auth');
       browser.driver.findElement(by.id('entrar')).click();
       browser.driver.findElement(by.id('login_field')).sendKeys(config.seleniumUser);
       browser.driver.findElement(by.id('password')).sendKeys(config.seleniumUserPassword);
